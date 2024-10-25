e830ce42a445cbdf2cfb33effc69fb3e09d4636f
Anwar Hermuche <anwarhermuche2@gmail.com>
2024-10-25 12:17:15 -0300

    Criação da classe C

---
+++ C.py
@@ -0,0 +1,11 @@
+class C:
+
+    def __init__(self, c1: str, c2: int):
+        self.c1 = c1
+        self.c2 = c2
+
+    def mc1(self) -> None:
+        print("Método MC1")
+
+    def mc2(self) -> None:
+        print("Método MC2")
