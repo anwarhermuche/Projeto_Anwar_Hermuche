4838b614c9a14d31d8d469d70f7e751c265ea597
Anwar Hermuche <anwarhermuche2@gmail.com>
2024-10-25 12:39:06 -0300

    Criação da classe D

---
+++ D.py
@@ -0,0 +1,11 @@
+class D:
+
+    def __init__(self, d1: str, d2: str):
+        self.d1 = d1
+        self.d2 = d2
+
+    def md1(self) -> None:
+        print("Método MD1")
+
+    def md2(self) -> None:
+        print("Método MD2")
